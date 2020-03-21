5767db23071a9b31712dda897ccd2365ab31e7c6
bluehole333 <bluehole333@foxmail.com>
2020-03-21 23:41:40 +0800

    可用属性

--- serializers.py
+++ serializers.py
@@ -3,10 +3,30 @@ from rest_framework import serializers
 """
 serializers 可用字段
 
-BooleanField	BooleanField()
-NullBooleanField	NullBooleanField()
-CharField	CharField(max_length=None, min_length=None, allow_blank=False, trim_whitespace=True)
-EmailField	EmailField(max_length=None, min_length=None, allow_blank=False)
+BooleanField	        BooleanField()
+NullBooleanField	    NullBooleanField()
+CharField	            CharField(max_length=None, min_length=None, allow_blank=False, trim_whitespace=True)
+EmailField	            EmailField(max_length=None, min_length=None, allow_blank=False)
+RegexField          	RegexField(regex, max_length=None, min_length=None, allow_blank=False)
+SlugField	            SlugField(maxlength=50, min_length=None, allow_blank=False) 正则字段，验证正则模式 [a-zA-Z0-9-]+
+URLField	            URLField(max_length=200, min_length=None, allow_blank=False)
+UUIDField	            UUIDField(format='hex_verbose') 
+                        format: 
+                            1) 'hex_verbose' 如"5ce0e9a5-5ffa-654b-cee0-1238041fb31a" 
+                            2） 'hex' 如 "5ce0e9a55ffa654bcee01238041fb31a" 
+                            3）'int' - 如: "123456789012312313134124512351145145114" 
+                            4）'urn' 如: "urn:uuid:5ce0e9a5-5ffa-654b-cee0-1238041fb31a"
+IPAddressField	        IPAddressField(protocol='both', unpack_ipv4=False, **options)
+IntegerField	        IntegerField(max_value=None, min_value=None)
+FloatField	            FloatField(max_value=None, min_value=None)
+DecimalField	        DecimalField(max_digits, decimal_places, coerce_to_string=None, max_value=None, min_value=None)
+                        max_digits: 最多位数
+                        decimal_palces: 小数点位置
+DateTimeField	        DateTimeField(format=api_settings.DATETIME_FORMAT, input_formats=None)
+DateField	            DateField(format=api_settings.DATE_FORMAT, input_formats=None)
+TimeField	            TimeField(format=api_settings.TIME_FORMAT, input_formats=None)
+DurationField	        DurationField()
+ChoiceField	            ChoiceField(choices) choices与Django的用法相同
 
 """
 
