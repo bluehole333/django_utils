4d6224a54303dbba631b3d5dd67497354f09ce28
bluehole333 <bluehole333@foxmail.com>
2020-05-29 11:50:39 +0800

    自定义exceptions

---
+++ exceptions.py
@@ -0,0 +1,26 @@
+import logging
+from rest_framework.views import exception_handler
+
+logger = logging.getLogger('request_data')
+
+
+def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    # Now add the HTTP status code to the response.
+    if response is not None:
+        data = {'errcode': response.status_code}
+
+        errmsg = {}
+        for key, value in response.data.items():
+            if type(value) == list:
+                errmsg[key] = value[0]
+            else:
+                errmsg[key] = value
+
+        data['errmsg'] = errmsg
+        response.data = data
+
+    return response
